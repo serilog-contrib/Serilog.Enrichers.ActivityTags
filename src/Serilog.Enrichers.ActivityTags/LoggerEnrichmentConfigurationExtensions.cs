f26aeb222cc2c8ddd30a7b75822a514c7959df6f
SeungYongShim <syshim@me.com>
2021-05-08 09:43:01 +0900

    modify common.props

--- src/Serilog.Enrichers.ActivityTags/LoggerEnrichmentConfigurationExtensions.cs
+++ src/Serilog.Enrichers.ActivityTags/LoggerEnrichmentConfigurationExtensions.cs
@@ -4,18 +4,8 @@ using Serilog.Configuration;
 
 namespace Serilog.Enrichers.ActivityTags
 {
-   
-
-    /// <summary>
-    /// <see cref="LoggerEnrichmentConfiguration"/> extension methods.
-    /// </summary>
     public static class LoggerEnrichmentConfigurationExtensions
     {
-        /// <summary>
-        /// Enrich logger output with span information from the current <see cref="Activity"/>.
-        /// </summary>
-        /// <param name="loggerEnrichmentConfiguration">The enrichment configuration.</param>
-        /// <returns>Configuration object allowing method chaining.</returns>
         public static LoggerConfiguration WithActivityTags(this LoggerEnrichmentConfiguration loggerEnrichmentConfiguration)
         {
             if (loggerEnrichmentConfiguration is null)
